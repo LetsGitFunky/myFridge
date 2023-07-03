e1ea8d8d7d9682ebccd8d1617224fa713f23698d
nj <nickgentrymma@gmail.com>
2023-07-03 14:46:31 -0700

    added validation for Ingredient.

---
+++ backend/validations/ingredients.js
@@ -0,0 +1,13 @@
+// validations/recipes.js
+
+const { check } = require("express-validator");
+const handleValidationErrors = require('./handleValidationErrors');
+
+
+const validateIngredient = [
+    check('name')
+        .exists({checkFalsy: true}),
+        handleValidationErrors,
+];
+
+module.exports = validateIngredient;
