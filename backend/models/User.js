6bc3d2f60fae880848708be62aff9d8e9706d6af
Matthew Montejo <montejododger@yahoo.com>
2023-07-03 14:36:05 -0700

    model User updated

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,21 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        hashedPassword: {
+            type: String,
+            required: true,
+        }, //fridge is an array of ObjectIds, each of which references an Ingredient
+        fridge: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Ingredients',
+        }],//savedRecipes is an array of ObjectIds, each of which references a Recipe
+        savedRecipes: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Recipe',
+        }]
     },
-    email: {
-        type: String,
-        required: true
-    },
-    hashedPassword: {
-        type: String,
-        required: true
+    {
+        timestamps: true,
     }
-}, {
-    timestamps: true
-});
+);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
