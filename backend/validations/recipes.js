5ad99d35b872716b1eb949834ff227937f19a0d4
nj <nickgentrymma@gmail.com>
2023-07-03 14:21:29 -0700

    added recipe validations.

---
+++ backend/validations/recipes.js
@@ -0,0 +1,27 @@
+// validations/recipes.js
+
+const { check } = require("express-validator");
+const handleValidationErrors = require('./handleValidationErrors');
+
+
+const validateRecipeInput = [
+    check('ingredients')
+        .exists({checkFalsy: true}),
+        handleValidationErrors,
+
+    check('instructions')
+        .exists({ checkFalsy: true }),
+        handleValidationErrors,
+        
+    check('step')
+        .exists({ checkFalsy: true })
+        .isNumeric(),   // auto fill from vs code to ensure step is a number.
+        handleValidationErrors,  // since recipes will be coming from chat no custom error messages?
+    
+    check('description')
+        .exists({ checkFalsy: true }),
+        handleValidationErrors, // no max length.
+
+];
+
+module.exports = validateRecipeInput;
