e8f597be25f7a2e64cb813e1f858e3d8906bfb3c
hannadarwish <hannadarwish@gmail.com>
2023-07-03 14:25:46 -0700

    added name key to recipeSchema

--- backend/models/Recipe.js
+++ backend/models/Recipe.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
     ingredients: [
         {
             type: Schema.Types.ObjectId,
