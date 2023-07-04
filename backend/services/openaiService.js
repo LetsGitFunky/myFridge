f43397b9e2e4384821b13e54c0b4f5ae02d0ffe9
LetsGitFunky <NickHeinBass@gmail.com>
2023-07-04 13:27:49 -0700

    hid api key

--- backend/services/openaiService.js
+++ backend/services/openaiService.js
@@ -5,8 +5,7 @@ const openai = axios.create({
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
-        // "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
-        "Authorization": 'Bearer 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'
+        "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
     },
 });
 
