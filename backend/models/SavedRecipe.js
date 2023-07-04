a8b9fa7801c6a6f12af0c73791abb43b588c2f5d
nj <nickgentrymma@gmail.com>
2023-07-03 17:32:49 -0700

    Created routes for savedRecipes

--- backend/models/SavedRecipe.js
+++ backend/models/SavedRecipe.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const savedRecipeSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
     name: {
         type: String,
         required: true
