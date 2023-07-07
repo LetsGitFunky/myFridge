9edb4239752a7aef7e2a2d42f85f403594238c04
LetsGitFunky <NickHeinBass@gmail.com>
2023-07-06 20:01:55 -0700

    generate recipes from ingredients index

--- frontend/src/components/IngredientsIndex/IngredientsIndex.jsx
+++ frontend/src/components/IngredientsIndex/IngredientsIndex.jsx
@@ -1,7 +1,8 @@
 import React  from "react";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchIngredients, getIngredients } from "../../store/ingredients";
+import { fetchIngredients } from "../../store/ingredients";
+import { fetchRecipes } from "../../store/recipes"
 
 
 export default function IngredientsIndex() {
@@ -9,21 +10,46 @@ export default function IngredientsIndex() {
     // const [ingredients, setIngredients] = useState("")
     const dispatch = useDispatch();
     const ings = useSelector(state => state.ingredients) || [];
+    const [selectedIngredients, setSelectedIngredients] = useState([]);
+
 
     useEffect(() => {
         dispatch(fetchIngredients(sessionUser._id));
     }, [dispatch, sessionUser._id, ings.length]);
 
-    // console.log(ings)
-    // debugger
-        return (
+    const handleInputChange = (event) => {
+        const target = event.target;
+        const value = target.name;
+
+        if(target.checked){
+            setSelectedIngredients(prevIngredients => [...prevIngredients, value])
+        } else {
+            setSelectedIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient !== value))
+        }
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        // Call fetchRecipes here with selectedIngredients as parameter
+        dispatch(fetchRecipes(selectedIngredients));
+    };
+
+    return (
         <div className="recipe-index-wrapper">
-                {Array.isArray(ings) && ings.map(ingredient => (
-                <li className="ingredient-item">{ingredient}</li>
+            <h1>My Ingredients:</h1>
+            <form onSubmit={handleSubmit}>
+                {Array.isArray(ings) && ings.map((ingredient, i) => (
+                    <div key={i}>
+                        <input
+                            name={ingredient}
+                            type="checkbox"
+                            onChange={handleInputChange}
+                        />
+                        <label>{ingredient}</label>
+                    </div>
                 ))}
+                <button type="submit">Generate Recipes</button>
+            </form>
         </div>
     )
-
-
-
 }
