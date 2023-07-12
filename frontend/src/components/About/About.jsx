1ce35f0da8cb8ecb9acd7a28e97603cfaa2679c4
hannadarwish <hannadarwish@gmail.com>
2023-07-12 13:36:44 -0700

    about me page

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
     return (
         <div className='about-me-page'>
             <div className='team-image-container'>
-                HELLO
+                The myFridge Team
                 <img src="your-image-url" alt="Team" />
             </div>
             <div className='team-links'>
@@ -20,7 +20,7 @@ export default function About() {
                             <a target="_blank" href="https://github.com/hannadarwish" className="icon-link">
                                 <AiFillGithub className="github-icon" />
                             </a>
-                            <a target="_blank" href="https://github.com/hannadarwish" className="icon-link">
+                            <a target="_blank" href="https://www.linkedin.com/in/hanna-darwish-89a34777/" className="icon-link">
                                 <AiFillLinkedin className="linkedin-icon" />
                             </a>
                         </div>
@@ -34,7 +34,7 @@ export default function About() {
                             <a target="_blank" href="https://github.com/NickGentryBJJ" className="icon-link">
                                 <AiFillGithub className="github-icon" />
                             </a>
-                            <a target="_blank" href="https://github.com/hannadarwish" className="icon-link">
+                            <a target="_blank" href="https://www.linkedin.com/in/nicholas-gentry-2721451b2/" className="icon-link">
                                 <AiFillLinkedin className="linkedin-icon" />
                             </a>
                         </div>
@@ -48,7 +48,7 @@ export default function About() {
                             <a target="_blank" href="https://github.com/LetsGitFunky" className="icon-link">
                                 <AiFillGithub className="github-icon" />
                             </a>
-                            <a target="_blank" href="https://github.com/hannadarwish" className="icon-link">
+                            <a target="_blank" href="https://www.linkedin.com/in/nickjhein/" className="icon-link">
                                 <AiFillLinkedin className="linkedin-icon" />
                             </a>
                         </div>
@@ -62,7 +62,7 @@ export default function About() {
                             <a target="_blank" href="https://github.com/montejododger" className="icon-link">
                                 <AiFillGithub className="github-icon" />
                             </a>
-                            <a target="_blank" href="https://github.com/hannadarwish" className="icon-link">
+                            <a target="_blank" href="https://www.linkedin.com/in/matthew-m-640905239/" className="icon-link">
                                 <AiFillLinkedin className="linkedin-icon" />
                             </a>
                         </div>
